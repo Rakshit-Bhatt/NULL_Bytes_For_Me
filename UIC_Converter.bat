ecdffab0ddd6855cb8627c15c8952b15b261676d
Rakshit Bhatt <Rakshitbhatt221@protonmail.com>
2021-07-06 22:10:47 +0530

    Add files via upload
    A Windows .bat file to convert the .ui file into .py file without the old rusty cmd command method taken. Its simple to use for the conversion. Keep in mind to put it into the same directory of your source code.

---
+++ UIC_Converter.bat
@@ -0,0 +1,36 @@
+
+@echo off
+title .UI to .py files converter !
+echo Generate Python files from .UI files!
+
+pause
+echo ..................................
+
+echo .UI file Name
+set /p UiName=Enter .UI file Name: 
+echo ..................................
+
+echo .PY file Name
+set /p PyName=Enter .PY file Name: 
+echo ..................................
+
+echo Started Converting File. Please wait.........
+
+
+
+call python -m PyQt5.uic.pyuic -x "%UiName%" -o "%PyName%"
+
+echo .QRC file Name
+set /p QrName=Enter .qrc file Name: 
+echo ..................................
+
+echo .PY file Name
+set /p PiName=Enter .PY file Name: 
+echo ..................................
+
+echo Started Converting Files. Please wait........
+
+pyrcc5 -o "%PiName%" "%QrName%"
+
+echo Job Completed.
+pause
